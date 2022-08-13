a848adb0b6d9e6fc86f4b9e189e1f2b671a217eb
Igor <Drinkinswo@gmail.com>
2022-08-13 22:17:08 +0300

    Утверждения с примитивными типами [#504883]

---
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BoxTest {
+
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isNotEqualTo("Cube")
+                        .isNotEmpty()
+                        .isEqualTo("Sphere");
+    }
+
+    @Test
+    void isThisUnknown() {
+        Box box = new Box(3, 10);
+        String name = box.whatsThis();
+        assertThat(name).isNotEmpty()
+                        .containsWhitespaces()
+                        .isEqualTo("Unknown object");
+    }
+
+    @Test
+    void whenVertex6ThenVertices6() {
+        Box box = new Box(6, 10);
+        int count = box.getNumberOfVertices();
+        assertThat(count).isEven()
+                         .isPositive()
+                         .isEqualTo(6);
+    }
+
+    @Test
+    void figureExists() {
+        Box box = new Box(4, 10);
+        boolean result = box.isExist();
+        assertThat(result).isEqualTo(true)
+                          .isTrue();
+    }
+
+    @Test
+    void sphereArea() {
+        Box box = new Box(4, 10);
+        double result = box.getArea();
+        assertThat(result).isNotZero()
+                          .isBetween(100.0, 200.0)
+                          .isEqualTo(173.205, withPrecision(0.01));
+    }
+
+    @Test
+    void noFigureArea() {
+        Box box = new Box(3, 10);
+        double result = box.getArea();
+        assertThat(result).isNotNaN()
+                          .isZero();
+    }
+}
