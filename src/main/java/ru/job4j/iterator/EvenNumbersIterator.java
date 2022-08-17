9ac1d6d86b95c8171b802cbcde4a1cf48522c0ba
Igor <Drinkinswo@gmail.com>
2022-08-17 20:47:33 +0300

    5.1.2.Итератор четных чисел [#150] fix#3

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -5,8 +5,7 @@ import java.util.*;
 public class EvenNumbersIterator implements Iterator<Integer> {
 
     private final int[] data;
-    private int evenIndex;
-    private int index = -1;
+    private int index;
 
     public EvenNumbersIterator(int[] data) {
         this.data = data;
@@ -14,16 +13,10 @@ public class EvenNumbersIterator implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        boolean b = false;
-        int pos = index;
-        while ((pos < data.length - 1) && !b) {
-            pos++;
-            if (data[pos] % 2 == 0) {
-                evenIndex = pos;
-                b = true;
+        while (index < data.length  && data[index] % 2 != 0) {
+                index++;
             }
-        }
-        return b;
+        return index < data.length;
     }
     
     @Override
@@ -31,7 +24,6 @@ public class EvenNumbersIterator implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        index = evenIndex;
-        return data[index];
+        return data[index++];
     }
 }
