6a71e3f085119ec9a7803ccc9912836f7f46a705
Igor <Drinkinswo@gmail.com>
2022-09-10 15:23:40 +0300

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analyze.java
@@ -0,0 +1,24 @@
+package ru.job4j.question;
+
+import java.util.*;
+
+public class Analyze {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        Info info = new Info();
+        Map<Integer, String> diffMap = new HashMap<>();
+        for (User prev : previous) {
+            diffMap.put(prev.getId(), prev.getName());
+        }
+        for (User curr : current) {
+            if (!diffMap.containsKey(curr.getId())) {
+                info.setAdded(info.getAdded() + 1);
+            } else if (!curr.getName()
+                            .equals(diffMap.get(curr.getId()))) {
+                info.setChanged(info.getChanged() + 1);
+            }
+            info.setDeleted(previous.size() - current.size() + info.getAdded());
+        }
+        return info;
+    }
+}
