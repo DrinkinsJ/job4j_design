bb8f532323f88583cc5a31d0d81cf703b45d81bd
Igor <Drinkinswo@gmail.com>
2022-09-14 23:25:20 +0300

    2. Анализ доступности сервера. [#859 #343415] fix#3

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,15 +1,11 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.*;
 
 public class Analizy {
     private boolean flag = true;
     private final StringBuilder sb = new StringBuilder();
-    private final List<String> offStatus = Arrays.asList("400", "500");
-    private final List<String> onStatus = Arrays.asList("200", "300");
-
-
+    
     public static void main(String[] args) {
         Analizy analizy = new Analizy();
         analizy.unavailable("data/server.log", "data/unavailable.csv");
@@ -21,13 +17,10 @@ public class Analizy {
               .filter(e -> !e.isEmpty() && e.contains(" "))
               .map(e -> e.split(" ", 2))
               .forEach(e -> {
-                  if (offStatus.contains(e[0]) && flag) {
-                      flag = false;
-                      sb.append(e[1]).append(";");
-                  }
-                  if (onStatus.contains(e[0]) && !flag) {
-                      flag = true;
-                      sb.append(e[1]).append(System.lineSeparator());
+                  if (("400".equals(e[0]) || "500".equals(e[0])) == flag) {
+                      sb.append(e[1])
+                        .append(flag ? ';' : System.lineSeparator());
+                      flag = !flag;
                   }
               });
         } catch (IOException e) {
