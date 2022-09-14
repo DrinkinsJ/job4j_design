5c6efa551a02f96540e61b6b20c9d6b5449b6685
Igor <Drinkinswo@gmail.com>
2022-09-14 16:04:48 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,38 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analizy {
+    boolean flag = true;
+    StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Analizy analizy = new Analizy();
+        analizy.unavailable("data/server.log", "data/unavailable.csv");
+    }
+
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            in.lines()
+              .map(e -> e.split(" ", 2))
+              .forEach(e -> {
+                  if ((e[0].equals("400") || e[0].equals("500")) && flag) {
+                      flag = false;
+                      sb.append(e[1])
+                        .append(";");
+                  }
+                  if ((e[0].equals("200") || e[0].equals("300")) && !flag) {
+                      flag = true;
+                      sb.append(e[1]).append(System.lineSeparator());
+                  }
+              });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            out.print(sb);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
