95976267da418dc265f1edf9e387d3fb15f7ed85
Igor <Drinkinswo@gmail.com>
2022-09-27 14:47:39 +0300

    5. Валидация параметров запуска. [#246865 #346717] Fix#1

--- src/main/java/ru/job4j/io/search/Search.java
+++ src/main/java/ru/job4j/io/search/Search.java
@@ -1,29 +1,32 @@
 package ru.job4j.io.search;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (Objects.isNull(args[0]) || Objects.isNull(args[1])) {
+        if (args.length != 2) {
             throw new IllegalArgumentException("Root folder is null or file.name. Usage: -jar C:\\ txt ");
         }
         validate(args[0], args[1]);
     }
 
     public static void validate(String s1, String s2) throws IOException {
-        if (!s1.contains(":\\") || s1.isBlank()) {
-            throw new IllegalArgumentException("Not correct path");
+        File file = new File(s1);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
-        if (s2.isBlank()) {
-            throw new IllegalArgumentException("File name cant be empty");
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        Path start = Paths.get(s1);
+        if (!s2.startsWith(".")) {
+            throw new IllegalArgumentException("File format must start with dot");
+        }
+        Path start = Paths.get(file.getName());
         search(start, p -> p.toFile().getName().endsWith(s2)).forEach(System.out::println);
     }
 
