e4a99fa8bdc7039014c4145ce260eeb81bc264b9
Igor <Drinkinswo@gmail.com>
2022-10-15 19:15:35 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/xml/Main.java
+++ src/main/java/ru/job4j/serialization/xml/Main.java
@@ -1,12 +1,22 @@
 package ru.job4j.serialization.xml;
 
-import ru.job4j.serialization.java.Contact;
-import ru.job4j.serialization.json.Worker;
+import javax.xml.bind.*;
+import java.io.StringWriter;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
         final Worker worker = new Worker(true, 10, new Contact(654321, "+7(926)323-45-65"),
-                new String[] {"Java", "io", "serialization"});
-        System.out.println(worker);
+                new String[]{"Java", "io", "serialization"});
+        JAXBContext context = JAXBContext.newInstance(Worker.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(worker, writer);
+            String result = writer.getBuffer().toString();
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
