c15c236e9b5c30b18f0ca2667bf533ee00ae8efc
Dorokhin Igor <drinkinswo@gmail.com>
2022-10-23 21:59:46 +0300

    0. Select [#6861]

---
+++ src/psql_scripts/0. Select [#6861]/fauna_insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('fish', 5000, null);
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('fish', 5000, null);
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('cow', 10500, '1949-02-03');
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('cat', 50000, 'J51187');
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('dog', 13000, '1939-11-11');
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('crab', 11000, '1989-03-08');
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('bird', 13000, null);
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('rat', 23000, '1993-01-08');
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('monkey', 22000, '1299-01-08');
