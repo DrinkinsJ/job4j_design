e0828b4ba16698b8c46001bed705b28b62fb5b32
Dorokhin Igor <drinkinswo@gmail.com>
2022-10-23 23:36:12 +0300

    0. Select [#6861 #354367] fix1

--- src/psql_scripts/0. Select [#6861]/fauna.sql
+++ src/psql_scripts/0. Select [#6861]/fauna.sql
@@ -1,3 +1,10 @@
+create table fauna (
+    id serial primary key,
+    name text,
+    avg_age int,
+    discovery_date date
+);
+
 INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('fish', 5000, null);
 INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('fish', 5000, null);
 INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('cow', 10500, '1949-02-03');
@@ -7,3 +14,8 @@ INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('crab', 11000, '1989-03
 INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('bird', 13000, null);
 INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('rat', 23000, '1993-01-08');
 INSERT INTO fauna(name, avg_age, discovery_date) VALUES ('monkey', 22000, '1299-01-08');
+
+SELECT * FROM fauna WHERE name LIKE '%fish%';
+SELECT * FROM fauna WHERE avg_age > 10000 AND avg_age < 21000;
+SELECT * FROM fauna WHERE discovery_date IS NULL;
+SELECT * FROM fauna WHERE discovery_date < '01-01-1950';
