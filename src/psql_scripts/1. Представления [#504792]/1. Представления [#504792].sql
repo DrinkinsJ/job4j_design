957c95b0a47fe57605ec9ccf600d674e91f8cea3
Dorokhin Igor <drinkinswo@gmail.com>
2022-11-06 18:41:55 +0300

    2. Триггеры [#504803] fix#2

--- src/psql_scripts/1. Представления [#504792]/1. Представления [#504792].sql
+++ src/psql_scripts/1. Представления [#504792]/1. Представления [#504792].sql
@@ -67,14 +67,11 @@ INSERT INTO cars(name, body_id) VALUES ('Vagabond', 7);
 INSERT INTO cars(name, body_id) VALUES ('Eon', 8);
 
 CREATE VIEW show_all_cars_with_body_and_no_other_details AS
-SELECT c.name car_name, cb.name body, ce.name engine, ct.name transmission FROM cars c 
+SELECT c.name car_name, cb.name body, ce.name engine, ct.name transmission FROM cars c
 LEFT JOIN car_bodies cb ON cb.id = c.body_id
 LEFT JOIN car_engines ce ON ce.id = c.engine_id
 LEFT JOIN car_transmissions ct ON ct.id = c.transmission_id
-WHERE ce.name NOT IN (
-SELECT name FROM car_engines WHERE c.engine_id = ce.id)
-AND ct.name NOT IN (
-SELECT name FROM car_transmissions WHERE c.transmission_id = ct.id);
+WHERE ct.id IS NULL AND ce.id IS NULL;
 
 SELECT * FROM show_all_cars_with_body_and_no_other_details;
 
