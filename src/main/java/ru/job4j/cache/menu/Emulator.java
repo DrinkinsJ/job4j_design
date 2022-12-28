8675c6c5fbe596bfb25d9783416e953952e8c8d8
drinkinsj <Drinkinswo@gmail.com>
2022-12-28 22:57:59 +0300

    1. Реализация кеша на SoftReference [#1592 #375781] fix#1

--- src/main/java/ru/job4j/cache/menu/Emulator.java
+++ src/main/java/ru/job4j/cache/menu/Emulator.java
@@ -6,14 +6,16 @@ import java.util.Scanner;
 
 public class Emulator {
 
-    static private final String USER_CHOICE = """
+    static private final String USER_MENU = """
             1 - загрузить содержимое файла в кэш
             2 - получить содержимое файла из кэша
             0 - выход""";
 
     static private final String ASK_FILE_NAME = "Введите название файла: ";
-
     static private final String CACHED = "Файл загружен в кэш. ";
+    static private final int USER_CHOICE_EXIT = 0;
+    static private final int USER_CHOICE_SAVE = 1;
+    static private final int USER_CHOICE_LOAD = 2;
     static private final String EXIT = "Выход";
 
     public static void main(String[] args) {
@@ -30,24 +32,23 @@ public class Emulator {
     }
 
     private void start(DirFileCache dirFileCache, Scanner scanner) {
-        System.out.println(USER_CHOICE);
+        System.out.println(USER_MENU);
         boolean run = true;
         while (run) {
             int userChoice = Integer.parseInt(scanner.nextLine());
-            if (userChoice == 1) {
+            if (userChoice == USER_CHOICE_SAVE) {
                 System.out.println(ASK_FILE_NAME);
                 dirFileCache.get(scanner.nextLine());
                 System.out.println(CACHED);
-                System.out.println(USER_CHOICE);
-            } else if (userChoice == 2) {
+                System.out.println(USER_MENU);
+            } else if (userChoice == USER_CHOICE_LOAD) {
                 System.out.println(ASK_FILE_NAME);
                 System.out.println(dirFileCache.get(scanner.nextLine()));
-                System.out.println(USER_CHOICE);
-            } else if (userChoice == 0) {
+                System.out.println(USER_MENU);
+            } else if (userChoice == USER_CHOICE_EXIT) {
                 System.out.println(EXIT);
                 run = false;
             }
         }
-
     }
 }
