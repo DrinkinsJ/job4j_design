9133560a47f60f583560015a4f46bdc8e5abb670
drinkinsj <Drinkinswo@gmail.com>
2023-01-08 20:53:56 +0300

    move package fix#2

--- src/main/java/ru/job4j/ood/srp/srpViolationExamples/DocScanAndSender.java
+++ src/main/java/ru/job4j/ood/srp/srpViolationExamples/DocScanAndSender.java
@@ -1,4 +1,4 @@
-package ru.job4j.ood.srp;
+package ru.job4j.ood.srp.srpViolationExamples;
 
 /**
  * Класс правильно реализует методы интерфейсов согласно SRP. Проблема в том, что в классе имеется конструктор
